52f50e97cd5853090a0f6a08c239e58a4f964e19
fhuszti <f.huszti@gmail.com>
2025-05-11 14:24:50 +0200

    added ci

--- test/integration/main_test.go
+++ test/integration/main_test.go
@@ -11,24 +11,68 @@ import (
 var GlobalMinioClient *storage.Strg
 
 func TestMain(m *testing.M) {
+	code := func() int {
+		dbCleanup, err := setupMariaDB()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "DB setup failed: %v\n", err)
+			return 1
+		}
+		defer dbCleanup()
+
+		minioCleanup, err := setupMinIO()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "MinIO setup failed: %v\n", err)
+			return 1
+		}
+		defer minioCleanup()
+
+		return m.Run()
+	}()
+
+	os.Exit(code)
+}
+
+func setupMariaDB() (cleanup func(), err error) {
+	if os.Getenv("TEST_DB_DSN") != "" {
+		// CI provided it; nothing to clean up
+		return func() {}, nil
+	}
+
 	mdb, err := testutil.StartMariaDBContainer()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to start MariaDB: %v\n", err)
-		os.Exit(1)
+		return nil, err
 	}
+
 	os.Setenv("TEST_DB_DSN", mdb.DSN)
 
-	minio, err := testutil.StartMinIOContainer()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to start MinIO: %v\n", err)
-		os.Exit(1)
+	return mdb.Cleanup, nil
+}
+
+func setupMinIO() (cleanup func(), err error) {
+	if os.Getenv("TEST_MINIO_ENDPOINT") != "" {
+		// CI path: build the global Strg client
+		endpoint := os.Getenv("TEST_MINIO_ENDPOINT")
+		access := os.Getenv("TEST_MINIO_ACCESS_KEY")
+		secret := os.Getenv("TEST_MINIO_SECRET_KEY")
+		useSSL := os.Getenv("TEST_MINIO_USE_SSL") == "true"
+
+		client, err := storage.NewMinioClient(endpoint, access, secret, useSSL)
+		if err != nil {
+			return nil, err
+		}
+
+		GlobalMinioClient = client
+
+		return func() {}, nil
 	}
-	GlobalMinioClient = minio.Client
 
-	exitCode := m.Run()
+	// local path: start a container
+	mi, err := testutil.StartMinIOContainer()
+	if err != nil {
+		return nil, err
+	}
 
-	minio.Cleanup()
-	mdb.Cleanup()
+	GlobalMinioClient = mi.Client
 
-	os.Exit(exitCode)
+	return mi.Cleanup, nil
 }
