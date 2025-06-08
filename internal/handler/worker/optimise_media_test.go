b0584b014a6fae3ac975fd3243a495e745c1ef42
François Huszti <f.huszti@gmail.com>
2025-06-08 15:55:48 +0200

    Add unit tests for optimise media worker

---
+++ internal/handler/worker/optimise_media_test.go
@@ -0,0 +1,68 @@
+package worker
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/fhuszti/medias-ms-go/internal/db"
+	"github.com/fhuszti/medias-ms-go/internal/task"
+	mediaSvc "github.com/fhuszti/medias-ms-go/internal/usecase/media"
+	"github.com/google/uuid"
+)
+
+type mockOptimiser struct {
+	in     mediaSvc.OptimiseMediaInput
+	called bool
+	err    error
+}
+
+func (m *mockOptimiser) OptimiseMedia(ctx context.Context, in mediaSvc.OptimiseMediaInput) error {
+	m.called = true
+	m.in = in
+	return m.err
+}
+
+func TestOptimiseMediaHandler_InvalidID(t *testing.T) {
+	svc := &mockOptimiser{}
+	err := OptimiseMediaHandler(context.Background(), task.OptimiseMediaPayload{MediaID: "invalid"}, svc)
+	if err == nil {
+		t.Fatal("expected error for invalid UUID")
+	}
+	if svc.called {
+		t.Error("service should not be called on invalid id")
+	}
+}
+
+func TestOptimiseMediaHandler_ServiceError(t *testing.T) {
+	id := uuid.New()
+	svcErr := errors.New("svc fail")
+	svc := &mockOptimiser{err: svcErr}
+
+	err := OptimiseMediaHandler(context.Background(), task.OptimiseMediaPayload{MediaID: id.String()}, svc)
+	if !errors.Is(err, svcErr) {
+		t.Fatalf("got error %v; want %v", err, svcErr)
+	}
+	if !svc.called {
+		t.Error("service not called")
+	}
+	if svc.in.ID != db.UUID(id) {
+		t.Errorf("service got id %s; want %s", svc.in.ID, id)
+	}
+}
+
+func TestOptimiseMediaHandler_Success(t *testing.T) {
+	id := uuid.New()
+	svc := &mockOptimiser{}
+
+	err := OptimiseMediaHandler(context.Background(), task.OptimiseMediaPayload{MediaID: id.String()}, svc)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if !svc.called {
+		t.Error("service not called")
+	}
+	if svc.in.ID != db.UUID(id) {
+		t.Errorf("service got id %s; want %s", svc.in.ID, id)
+	}
+}
